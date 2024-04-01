5966670a965c8bc607f18d02b0222a6c0ec282cd
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2024-04-01 11:54:15 +0800

    Initial commit

---
+++ src/main/GameScreen.java
@@ -0,0 +1,21 @@
+package main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+
+public class GameScreen  extends JPanel{
+	
+	public GameScreen() {
+		
+	}
+	
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.setColor(Color.RED);
+		g.fillRect(50,50,100,100);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MuckTD {
+	requires java.desktop;
+}
