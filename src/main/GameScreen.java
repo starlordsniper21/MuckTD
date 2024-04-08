fe8a54c546f0bb85ddd66315f0276f1e7ae74667
dyrus <114223751+starlordsniper21@users.noreply.github.com>
2024-04-08 11:38:39 +0800

    Rendering Imgs

--- src/main/GameScreen.java
+++ src/main/GameScreen.java
@@ -2,20 +2,62 @@ package main;
 
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class GameScreen  extends JPanel{
 	
-	public GameScreen() {
+	private Random random;
+	private BufferedImage img;
+	private ArrayList<BufferedImage> sprites = new ArrayList<>();
+	
+	public GameScreen(BufferedImage img) {
+		this.img = img;
+		loadSprites();
+		random = new Random();
+	}
+	
+	private void loadSprites() {
+		
+		for (int y = 0; y < 10; y++) {
+			for (int x = 0; x < 10; x++) {
+				sprites.add(img.getSubimage(x * 32, y * 32, 32, 32));
+			}
+		}
 		
 	}
-	
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.setColor(Color.RED);
-		g.fillRect(50,50,100,100);
+		
+		
+//		g.drawImage(sprites.get(19), 0, 0, null);
+					
+		for (int y = 0; y < 20; y++) {
+			for (int x = 0; x < 20; x++) {
+				
+				g.drawImage(sprites.get(getRndInt()), x * 32, y* 32, null);
+				
+			}
+		}
 	}
+	
+	private int getRndInt() {
+		return random.nextInt(100);
+	}
+	
+	
+	private Color getRndColor() {
+		int r = random.nextInt(256);
+		int g = random.nextInt(256);
+		int b = random.nextInt(256);
+		
+		return new Color(r,g,b);
+	}
+	
+	
 
 }
